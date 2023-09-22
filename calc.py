5e7feb45f56f6a7a6f4bba6606253854f99348f0
mutugi001 <collinsmut04@gmail.com>
2023-09-22 15:40:26 +0300

    added calc.py

---
+++ calc.py
@@ -0,0 +1,37 @@
+
+name = input("enter name: ")
+print("hello " + name)
+
+num1 = input("enter num1: ")
+num1 = int(num1)
+opr = input("enter operator: ")
+num2 = input("enter num2: ")
+num2 = int(num2)
+
+if opr == "+":
+    sum = num1 + num2
+    print(sum)
+
+
+elif opr == "-":
+    diff = num1 - num2
+    print(diff)
+
+elif opr == "*":
+    mlt = num1 * num2
+    print(mlt)
+    
+elif opr == "%":
+    mod = num1 % num2
+    print(mod)
+
+elif opr == "/":
+    if num2 == "0":
+        print("division by zero!!")
+    else:
+        div = num1 / num2
+        print(div)
+        
+else:
+    print("error!!")
+    
